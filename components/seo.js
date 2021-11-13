99d16c7a1d6d4923a351e6539f1e663c69611818
noearaujo <araujonoe21@hotmail.com>
2021-11-13 12:14:06 -0300

    melhoria de SEO

--- components/seo.js
+++ components/seo.js
@@ -25,6 +25,31 @@ export default function SEO(props){
             <meta property="og:image:height" content="314" />
             <meta property="og:url" content={url} />
             <meta property="og:type" content="game" />
+
+            {/*<!-- Favicon and Apple Icons -->*/}
+            <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192"  href="/favicon/android-icon-192x192.png" />
+            <link rel="icon" type="image/ico" sizes="32x32" href="/favicon/favicon.ico" />
+
+           {/*!-- Global site tag (gtag.js) - Google Analytics -->*/}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150111827-1"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-150111827-1');
+            </script>
+
+            <link rel="manifest" href={`${uri}/manifest.json`} />
         </Head>
     );
 }
